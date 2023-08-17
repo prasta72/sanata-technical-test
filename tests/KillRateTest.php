d0d6c0316a43487877e25efd2ddc881e236800e7
prasta <prasta72@gmail.com>
2023-08-17 10:06:11 +0800

    fix unit test

--- tests/KillRateTest.php
+++ tests/KillRateTest.php
@@ -1,12 +1,15 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\Constraint\IsIdentical;
 use PHPUnit\Framework\TestCase;
 use src\KillRate;
 
 class KillRateTest extends TestCase {
-    public function testKillRateSuccess(){
-        $peoples  = [
+    public function testAverageKillRateWithTwoPeople()
+    {
+        $peoples = [
             'A' => [
                 'ageOfDeath' => 10,
                 'yearOfDeath' => 12
@@ -16,10 +19,39 @@ class KillRateTest extends TestCase {
                 'yearOfDeath' => 17
             ],
         ];
+        
         $killRate = new KillRate($peoples);
         $average = $killRate->getAverage();
-        $this->assertEquals(4.5, $average);
+        
+        $expectedAverage = 4.5;
+
+        $this->assertEquals($expectedAverage, $average);
     }
+
+    public function testAverageKillRateWithThreePeople()
+    {
+        $peoples = [
+            'A' => [
+                'ageOfDeath' => 10,
+                'yearOfDeath' => 12
+            ],
+            'B' => [
+                'ageOfDeath' => 13,
+                'yearOfDeath' => 17
+            ],
+            'C' => [
+                'ageOfDeath' => 10,
+                'yearOfDeath' => 15
+            ],
+        ];
+
+        $killRate = new KillRate($peoples);
+        $average = $killRate->getAverage();
+
+        $expectedAverage = 7;
+        $this->assertEquals($expectedAverage, $average);
+    }
+   
     public function testNegativeAge(){
         $peoples  = [
             'A' => [
